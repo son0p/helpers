d4cab25160bce0f35d49fd042ce84408f6cbd5aa
fede2001 <fede2001@hotmail.com>
2015-10-16 09:46:26 -0500

    agrego la función para consultar solo un mes, HACER: reordenar el resultado

--- calendar.R
+++ calendar.R
@@ -4,6 +4,7 @@ require(lubridate)
 fecha <- ymd("2015-01-01")
 fecha2 <- fecha + days(2)
 inte <- interval(fecha, fecha2 )
+mes <- interval( now(),(now()+days(30)) )
 
 calendario <- read.csv2("~/Dropbox/tools/calendario.csv")
 proximos_eventos <- function(calendario) {
@@ -11,7 +12,13 @@ proximos_eventos <- function(calendario) {
     calendario[ymd_hms(calendario$fecha) > hoy, ] ## Con horas es ymd_hms()
 }
 
+proximo_mes <- function(calendario) {
+    hoy <- now()
+    calendario[ymd_hms(calendario$fecha) %within% mes, ]
+}
+# Consultas
 proximos_eventos(calendario)
+proximo_mes(calendario)
 
 fecha3 %within% inte
 
