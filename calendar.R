2aeb2222d98df646b812876f6800df68d3b5056b
fede2001 <fede2001@hotmail.com>
2015-10-17 12:25:11 -0500

    consulta por mes

--- calendar.R
+++ calendar.R
@@ -1,4 +1,6 @@
 install.packages("lubridate")
+install.packages("DT")
+require("DT")
 require(lubridate)
 
 fecha <- ymd("2015-01-01")
@@ -7,6 +9,7 @@ inte <- interval(fecha, fecha2 )
 mes <- interval( now(),(now()+days(30)) )
 
 calendario <- read.csv2("~/Dropbox/tools/calendario.csv")
+calendario$fecha <- ymd_hms(calendario$fecha)
 proximos_eventos <- function(calendario) {
     hoy <- now()
     calendario[ymd_hms(calendario$fecha) > hoy, ] ## Con horas es ymd_hms()
@@ -14,11 +17,19 @@ proximos_eventos <- function(calendario) {
 
 proximo_mes <- function(calendario) {
     hoy <- now()
-    calendario[ymd_hms(calendario$fecha) %within% mes, ]
+    result <- calendario[ymd_hms(calendario$fecha) %within% mes, ]
+    result[order(result$fecha),]
 }
+
 # Consultas
 proximos_eventos(calendario)
+
 proximo_mes(calendario)
+View(calendario)
+datatable(calendario, filter = 'top',options = list(autoWidth = TRUE))
+
+require("html-widgets")
+
 
 fecha3 %within% inte
 
