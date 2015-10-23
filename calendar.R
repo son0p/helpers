d6f98c42b5a7815bc74e1257734c93ed79dd2f31
fede2001 <fede2001@hotmail.com>
2015-10-23 05:39:24 -0500

    Nuevas consultas

--- calendar.R
+++ calendar.R
@@ -1,20 +1,19 @@
 install.packages("lubridate")
 install.packages("DT")
+# ------- Reuquire
 require("DT")
 require(lubridate)
-
-fecha <- ymd("2015-01-01")
-fecha2 <- fecha + days(2)
-inte <- interval(fecha, fecha2 )
+# vars
 mes <- interval( now(),(now()+days(30)) )
-
+# data
 calendario <- read.csv2("~/Dropbox/tools/calendario.csv")
+# types
 calendario$fecha <- ymd_hms(calendario$fecha)
+# funciones
 proximos_eventos <- function(calendario) {
     hoy <- now()
     calendario[ymd_hms(calendario$fecha) > hoy, ] ## Con horas es ymd_hms()
 }
-
 proximo_mes <- function(calendario) {
     hoy <- now()
     result <- calendario[ymd_hms(calendario$fecha) %within% mes, ]
@@ -22,15 +21,30 @@ proximo_mes <- function(calendario) {
 }
 
 # Consultas
-proximos_eventos(calendario)
+futuro <- proximos_eventos(calendario)
+mes <- proximo_mes(calendario)
 
-proximo_mes(calendario)
+# Ver Calendario en consola
 View(calendario)
+# Ver Todo el calendario en el navegador
 datatable(calendario, filter = 'top',options = list(autoWidth = TRUE))
+# Ver todo el futuro en el navegador
+datatable(futuro, filter = 'top',options = list(autoWidth = TRUE))
+# Ver proximo mes en el navegador
+datatable(mes, filter = 'top',options = list(autoWidth = TRUE))
 
+# -----------
 require("html-widgets")
 
+fecha <- ymd("2015-01-01")
+fecha2 <- fecha + days(2)
+inte <- interval(fecha, fecha2 )
 
 fecha3 %within% inte
 
 fecha3 <- ymd("2015-01-02")
+
+# Orientacion
+
+tareas <- read.csv2("~/Dropbox/tools/orientacion.csv")
+datatable(tareas, filter = 'top',options = list(autoWidth = TRUE))
